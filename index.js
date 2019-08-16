d94a9f84613a5db3f48db53324a334e632c2eaa5
SilvioAmaral <silvio.amaral@gmail.com>
2019-08-16 11:12:07 -0400

    Testing recursion - fails

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import './style.css';
 const getCircularReplacer = () => {
   const seen = new WeakSet();
   return (key, value) => {    
-    console.log(key, value, Object.getOwnPropertyNames(value), Object.keys(value))
+    // console.log(key, value, Object.getOwnPropertyNames(value), Object.keys(value))
     if (typeof value === "object" && value !== null) {
       if (seen.has(value)) {
-        console.log('circ-ref', key)
+        // console.log('circ-ref', key)
         return '[Circ Ref]';
       }
       seen.add(value);
@@ -16,6 +16,27 @@ const getCircularReplacer = () => {
   };
 };
 
+var stringifyError = function(err, filter, space) {
+  var plainObject = {};
+  const seen = new WeakSet();
+  Object.getOwnPropertyNames(err).forEach(function(key) {
+    // console.log('str fn',  err[key], key)
+    const value = err[key];
+    console.log(typeof value, value);
+    if (typeof value === "object" && value !== null) {
+      console.log('in check', value)
+      if (seen.has(value)) {
+        // console.log('circ-ref', key)
+        plainObject[key] = '[Circ Ref]';
+      }
+      seen.add(value);
+    } 
+    plainObject[key] = value;
+  });
+  return JSON.stringify(plainObject, filter, space);
+};
+
+/** Object with circular reference */
 const obj = {
   prop1: 'silvio',
   prop2: 40,
@@ -23,30 +44,26 @@ const obj = {
     prop4: 'test'
   }
 }
-obj.prop3.prop5 = obj;
+// obj.prop3.prop5 = obj;
 
-var stringifyError = function(err, filter, space) {
-  var plainObject = {};
-  Object.getOwnPropertyNames(err).forEach(function(key) {
-      console.log('str fn',  err[key], key)
-    plainObject[key] = err[key];
-  });
-  return JSON.stringify(plainObject, filter, space);
-};
 
-const err = Error('t');
-err.message = 'abc';
-err.stack = ['1', '2'];
+/** Error object with circular reference */
+const err = new Error('t');
+err.message = 'abc'; // inherited prop
+err.stack = ['1', '2']; // inherited prop
 err.test = 'yeah';
-// err.message2 = obj;
+err.message2 = obj;
 console.info(err)
 
-// Write Javascript code!
+// The replacer works fine for regular objects
 const appDiv = document.getElementById('app');
 appDiv.innerText = JSON.stringify(obj, getCircularReplacer());
 
+// but it does not work for inherited members (error obj props message like or stack)
 const appDiv2 = document.getElementById('app2');
-appDiv2.innerText = stringifyError(err);
+appDiv2.innerText = JSON.stringify(err, getCircularReplacer());
 
+// so we will have to use a custom method on this case
 const appDiv3 = document.getElementById('app3');
-appDiv3.innerText = JSON.stringify(err, getCircularReplacer());
+appDiv3.innerText = stringifyError(err);
+
